4a3ac4fb2141aa314b739f48ebd071754364b765
CH HARSHA VARDHAN REDDY <harshasai1026@gmail.com>
2026-05-28 16:07:58 +0530

    Time: 12 ms (7.69%) | Memory: 46.6 MB (7.85%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,7 +2,10 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int rowSize = triangle.size();
         int colSize = 2;
+        int row[] = new int[rowSize];
+        row[0] = triangle.get(0).get(0);
         for(int i=1;i<rowSize;i++){
+            int minVal = Integer.MAX_VALUE;
             for(int j=0;j<colSize;j++){
                 if(j == 0){
                     int temp = triangle.get(i).get(j) + triangle.get(i-1).get(0);
@@ -15,16 +18,13 @@ class Solution {
                     int temp = Math.min(triangle.get(i-1).get(j-1) + current,triangle.get(i-1).get(j) + current);
                     triangle.get(i).set(j,temp);
                 }
+                minVal = Math.min(minVal,triangle.get(i).get(j));
                 // System.out.print(triangle.get(i).get(j) + " ");
             }
+            row[i] = minVal;
             // System.out.println();
             colSize++;
         }
-        int i = rowSize - 1;
-        int res = Integer.MAX_VALUE;
-        for(int j=0;j<colSize-1;j++){
-            res = Math.min(triangle.get(i).get(j) , res);
-        }
-        return res;
+        return row[rowSize-1];
     }
 }
