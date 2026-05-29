03593080a561a01480b3706d015375b2f07ca19b
CH HARSHA VARDHAN REDDY <harshasai1026@gmail.com>
2026-05-29 13:22:27 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (31.63%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,27 @@
+class Solution {
+    static {
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+        try (FileWriter writer = new FileWriter("display_runtime.txt")) {
+            writer.write("0");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }));
+}
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> hm = new HashMap<>();
+        for(int i:nums) hm.put(i,hm.getOrDefault(i,0)+1);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
+            return b[1] - a[1];
+        });
+        for(int i:hm.keySet()){
+            pq.add(new int[]{i,hm.get(i)});
+        }
+        int[] res = new int[k];
+        for(int i=0;i<k;i++){
+            int[] a = pq.poll();
+            res[i] = a[0];
+        }
+        return res;
+    }
+}
