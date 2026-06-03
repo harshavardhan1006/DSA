620b62d554dbba108eeb3fe21fd1d24264abf1a7
CH HARSHA VARDHAN REDDY <harshasai1026@gmail.com>
2026-06-03 08:58:44 +0530

    Time: 4 ms (39.65%) | Memory: 43.2 MB (71.66%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(!st.isEmpty() && ch == ')' && st.peek() == '(') st.pop();
+            else if(!st.isEmpty() && ch == ']' && st.peek() == '[') st.pop();
+            else if(!st.isEmpty() && ch == '}' && st.peek() == '{') st.pop();
+            else{
+                st.push(ch);
+            }
+        }
+        return st.isEmpty();
+    }
+}
